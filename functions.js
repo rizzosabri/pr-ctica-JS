3bab970029c156d9ea780da008e8dace320ad251
Sabrina Rizzo <sabri.rizzo@gmail.com>
2022-11-25 23:00:11 +0100

    ...

--- functions.js
+++ functions.js
@@ -3,3 +3,22 @@
     return Math.floor(Math.random() * max);
   }
   
+const students = [{
+  age: 32,
+  examScores: [],
+  gender: 'male',
+  name: 'Leodu'
+},
+{
+  age: 29,
+  examScores: [],
+  gender: 'female',
+  name: 'silvia'
+}]
+
+let arrAge=[]
+
+for (let i = 0; i <= students.length-1 ; i++) {
+  arrAge.push(students[i].age)
+}
+console.log(arrAge)
